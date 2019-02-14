45c4f5b739ad3d0d5a823a4a0661a3fac5613574
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-02-13 19:59:25 -0600

    updated views and controller and lists

--- Cozy.WebUI/Cozy.WebUI/Controllers/LandlordController.cs
+++ Cozy.WebUI/Cozy.WebUI/Controllers/LandlordController.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cozy.Domain.Models;
+using Cozy.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cozy.WebUI.Controllers
 {
     public class LandlordController : Controller
     {
+        private readonly IHomeService _homeService;
+        private readonly ILeaseService _leaseService;
+
+        public LandlordController(IHomeService homeService)=>
+            _homeService = homeService;
+
+        public LandlordController(ILeaseService leaseService) =>
+            _leaseService = leaseService;
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult HomeHistory(int id)
+        {
+            Home home = _homeService.GetById(id);
+            ICollection<Lease> leases = _leaseService.GetByHomeId(id);
             return View();
         }
     }
