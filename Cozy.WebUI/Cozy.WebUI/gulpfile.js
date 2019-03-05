a229b57681d52bdca59cc3745a4f4b0448c03fa8
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-03-04 20:02:42 -0600

    using the new files

--- Cozy.WebUI/Cozy.WebUI/gulpfile.js
+++ Cozy.WebUI/Cozy.WebUI/gulpfile.js
@@ -1,1 +1,11 @@
-﻿
+﻿'use strict';
+const sass = require('gulp-sass');
+
+function myTask(cb) {
+    //test
+    console.log('My first task.');
+    //call back function
+    cb();
+}
+
+exports.default = myTask;
